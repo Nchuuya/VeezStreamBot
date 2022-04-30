ddfca46441aab401729ee477a394733d7412ee5c
levina <82658782+levina-lab@users.noreply.github.com>
2022-04-30 20:56:02 +0700

    treewide: initial commit

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,1 +1,20 @@
-""" bot plugins """
+__version__ = '8.5 Beta'
+
+
+import time
+import logging
+
+from database.core import me_bot
+
+
+logging.BasicConfig(
+    filename=f'logs-{me_bot.id}.txt',
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+logging.getLogger('yt_dlp').setLevel(logging.ERROR)
+logging.getLogger('pyrogram').setLevel(logging.ERROR)
+logging.getLogger('pytgcalls').setLevel(logging.ERROR)
+
+
+LOGS = logging.getLogger(__name__)
