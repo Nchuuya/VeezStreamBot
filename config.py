1a93538f4009e143ab051d0dbe15275f3c9d1c15
levina <82658782+levina-lab@users.noreply.github.com>
2022-05-01 13:54:13 +0700

    treewide: initial commit

---
+++ config.py
@@ -0,0 +1,23 @@
+from os import getenv
+from dotenv import load_dotenv
+
+
+admins = {}
+load_dotenv()
+
+
+api_id = int(getenv("api_id"))
+api_hash = getenv("api_hash")
+token = getenv("token")
+session = getenv("session")
+
+creator_id = int(getenv("creator_id"))
+creator_name = getenv("creator_name")
+creator_username = getenv("creator_username")
+
+database_url = getenv("database_url")
+sudoers_id = list(map(int, getenv("sudoers_id").split()))
+cmd_handler = list(getenv("cmd_handler", "/ . $ !").split())
+
+support_group = getenv("support_group")
+support_channel = getenv("support_channel")
